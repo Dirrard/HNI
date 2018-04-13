8d73433a11e4ad53923eea386f3422dbb224dca3
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-04-12 21:01:47 -0300

    Index-Concluida

---
+++ HNI/HNI/Controllers/HNIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HNI.Controllers
+{
+    public class HNIController : Controller
+    {
+        // GET: HNI
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
