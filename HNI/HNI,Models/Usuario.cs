d1a49bd3de169137cc851f1dd60319264906ff11
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-04-17 20:06:09 -0300

    Criando Model

---
+++ HNI/HNI,Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_Models
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Nick { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string ConfSenha { get; set; }
+        public int DataNasc { get; set; }
+        public string Genero { get; set; }
+        public bool Termo { get; set; }
+    }
+}
