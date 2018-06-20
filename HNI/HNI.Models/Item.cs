852cced07cd4e042bf80f1f8c24f93fcca585d09
MatheusCristiano <36750514+MatheusCristiano@users.noreply.github.com>
2018-06-20 18:21:15 -0300

    alterações de diagramas e programa

---
+++ HNI/HNI.Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI.Models
+{
+    public class Item
+    {
+        public int Int { get; set; }
+        public string Imagem { get; set; }
+        public string Nome { get; set; }
+        public int Mana { get; set; }
+        public int hp { get; set; }
+        public int AtkF { get; set; }
+        public int AtkM { get; set; }
+        public int Def { get; set; }
+        public int Nivel { get; set; }
+        public int Valor { get; set; }
+
+    }
+}
