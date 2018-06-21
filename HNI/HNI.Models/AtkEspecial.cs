1cd95016d8cc889420fb0a9d8fb3c354ef055573
MatheusCristiano <36750514+MatheusCristiano@users.noreply.github.com>
2018-06-20 21:21:51 -0300

    alterações de diagramas

---
+++ HNI/HNI.Models/AtkEspecial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI.Models
+{
+    class AtkEspecial
+    {
+        public int Id { get; set; }
+        public string Imagem { get; set; }
+        public string Nome { get; set; }
+        public int AtkM { get; set; }
+        public int Def { get; set; }
+        public int AtkF { get; set; }
+        public int Hp { get; set; }
+        public int Mana { get; set; }
+    }
+}
